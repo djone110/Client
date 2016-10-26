de7647764dd36f9c2993858042d689cc440ebb91
BrandonDenton <oinkerbob1@gmail.com>
2016-10-26 19:48:49 -0400

    server setup and port definition

--- app/src/main/java/com/teamawesome/client/comm/commRecv.java
+++ app/src/main/java/com/teamawesome/client/comm/commRecv.java
@@ -1,11 +1,43 @@
 package com.teamawesome.client.comm;
 
+import java.net.*;  // sockets
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class commRecv extends BroadcastReceiver {
     public commRecv() {
+        // listen on port 42069 for training data from server
+        int portNumber = 42069;
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out =
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        )
+        //
+        String inputLine, outputLine;
+
+        // Initiate conversation with client
+        commRecvProtocol crp = new commRecvProtocol();
+        outputLine = crp.processInput(null);
+        out.println(outputLine);
+
+        while ((inputLine = in.readLine()) != null) {
+            outputLine = crp.processInput(inputLine);
+            out.println(outputLine);
+            if (outputLine.equals("some terminating stuff idk"))
+                break;
+        }
+    } catch (IOException e) {
+        System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+        System.out.println(e.getMessage());
+    }
+}
     }
 
     @Override
