8f7c2c6ec98bb24f5272f04c3444edf1523ff7f9
Matt Shindala <matt.shindala@gmail.com>
2016-11-08 18:27:01 -0500

    rework mainactivity

---
+++ app/src/main/java/com/teamawesome/client/main/AnalyzeResults.java
@@ -0,0 +1,15 @@
+package com.teamawesome.client.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.teamawesome.client.R;
+
+public class AnalyzeResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analyze_results);
+    }
+}
--- app/src/main/java/com/teamawesome/client/main/MainActivity.java
+++ app/src/main/java/com/teamawesome/client/main/MainActivity.java
@@ -18,64 +18,18 @@ import com.teamawesome.client.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button ;
-    Button jsonButton;
-    EditText editText;
-    String name;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            editText = (EditText) findViewById(R.id.editText);
-            button = (Button) findViewById(R.id.button);
-            jsonButton = (Button) findViewById(R.id.jsonButton);
-            int permissionLoc = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-             int permissionUsage = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.PACKAGE_USAGE_STATS);
-
-            if (permissionUsage != PackageManager.PERMISSION_GRANTED) {
-                startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
-                Toast.makeText(MainActivity.this, "We need Usage permission please. :)", Toast.LENGTH_SHORT).show();
-            }
-
-            if (permissionLoc != PackageManager.PERMISSION_GRANTED){
-                startActivity(new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS));
-                Toast.makeText(MainActivity.this, "Location permissions would be gr8!", Toast.LENGTH_SHORT).show();
-            }
-            Toast.makeText(MainActivity.this, "Please enter your name.", Toast.LENGTH_SHORT).show();
-
-            button.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    name = editText.getText().toString();
-                    if(name.length() == 0){
-                        Toast.makeText(MainActivity.this, "HEY! ENTER YOUR NAME.", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        Toast.makeText(MainActivity.this, name + " is a great name!", Toast.LENGTH_SHORT).show();
-                    }
-
-
-                    return false;
-                }
-            });
-
-        jsonButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-            Intent i = new Intent();
-                return false;
-            }
-        });
-
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
+    public void analyze(View v) {
+        Intent i = new Intent(this, AnalyzeResults.class);
+        startActivity(i);
+    }
 
-
-
+    public void clearHistory(View v) {
+        Toast.makeText(this, R.string.notImplementedYet, Toast.LENGTH_SHORT).show();
+    }
 }
