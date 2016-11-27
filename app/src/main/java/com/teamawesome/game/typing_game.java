ecf3075a6951a6542b2419929f11106fab22b92c
Sarah Schaffer <sschaff1@vol.utk.edu>
2016-11-27 14:20:11 -0500

    I worked on the typing speed game a little.

--- app/src/main/java/com/teamawesome/game/typing_game.java
+++ app/src/main/java/com/teamawesome/game/typing_game.java
@@ -1,23 +1,32 @@
 package com.teamawesome.game;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.teamawesome.client.R;
+
+import java.util.Arrays;
 
 /**
  * Created by Sarah on 11/15/16.
  **/
 
-public class typing_game extends AppCompatActivity implements View.OnClickListener{
+public class typing_game extends AppCompatActivity implements View.OnClickListener {
     Button done; //declare done button
     private EditText userInput; //declare user input variable
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     //create done button
     @Override
@@ -31,34 +40,86 @@ public class typing_game extends AppCompatActivity implements View.OnClickListen
         //create action listener
         done.setOnClickListener(typing_game.this);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //when done button is clicked
     @Override
     public void onClick(View v) {
         //get text from typing field
-        userInput = (EditText)findViewById(R.id.type_here);
+        userInput = (EditText) findViewById(R.id.type_here);
 
-        //alert user that data has been stored
-        //eventually this will let the user know if they need to retype
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setMessage("We stored your data.");
-        builder1.setCancelable(true);
-        builder1.setNeutralButton(
-                "Yes",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
+        //typing analysis
+        //store data
+        Boolean test = false;
+        String text = userInput.toString();
+        String[] ary = text.split(" ");
+        String[] copiedText = getApplicationContext().getResources().getString(R.string.typing_speed_text).split(" ");
 
-        //on button click, go to results of game (may change to next game)
-        startActivity(new Intent(typing_game.this, game_results.class));
+        test = Arrays.equals(ary, copiedText);
+
+        if (test == false) {
+            //alert user that data has been stored
+            //eventually this will let the user know if they need to retype
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("The text you entered does not match. Please correct your text to move forward.");
+            builder1.setCancelable(true);
+            builder1.setNeutralButton(
+                    "Yes",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+            return;
+        } else {
+            //on button click, go to results of game (may change to next game)
+            startActivity(new Intent(typing_game.this, game_results.class));
+        }
+
+    }
+}
+
+    /*
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    /*
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("typing_game Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
     }
 
-    //typing analysis
-    //store data
+    @Override
+    public void onStart() {
+        super.onStart();
 
-}
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+    */
+
